5e34524e840c4c4ded6fd6d18ba33e2489f92c8e
Antoine Jésus <antoine.jesus@telecomnancy.net>
2023-01-05 20:02:25 +0100

    Fix package issues and linter bugs

--- regis/src/components/Pages/Presenter.tsx
+++ regis/src/components/Pages/Presenter.tsx
@@ -1,11 +1,13 @@
+import { Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
 export const Presenter = (props: any) => {
   const navigate = useNavigate();
   return (
     <>
-      <h1>Ici les présentateurs</h1>
-      <button onClick={() => navigate(-1)}>back</button>
+      <Button variant='outlined' onClick={() => navigate(-1)}>
+        back
+      </Button>
     </>
   );
 };
